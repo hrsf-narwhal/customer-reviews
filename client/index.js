6d9448fefe63ea1b9a468c9f9127a458f7167f18
Aigerim <aigerim.zholmurzayeva@gmail.com>
2018-06-05 16:00:27 -0700

    Setup server endpoints

--- client/index.js
+++ client/index.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Reviews from './components/Reviews.jsx';
 
-window.Reviews = Reviews;
+
+
+
+ReactDOM.render(<Reviews/>, document.getElementById('reviews'));
