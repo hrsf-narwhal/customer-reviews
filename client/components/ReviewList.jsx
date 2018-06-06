86150577043487f09196d7fd9d76526945bae036
Aigerim <aigerim.zholmurzayeva@gmail.com>
2018-06-06 09:56:40 -0700

    Clean up code and fix indentation

--- client/components/ReviewList.jsx
+++ client/components/ReviewList.jsx
@@ -6,20 +6,22 @@ import ReviewListEntry from './ReviewListEntry.jsx';
 	return (
 		<div>
 			<table>
-		  <thead>
-			  <tr>
-			    <th>
-			     <i className="fas fa-star"></i>
-			    Reviews ***** ({props.reviews.length}) </th>
-			  </tr>
-		  </thead>
-		  <tbody> {props.reviews.map((review, i) => {
-		  	return (<ReviewListEntry key={i} review={review}/>)
-		  })}
-			</tbody>
-		</table>
+			  <thead>
+				  <tr>
+				    <th>
+				    	<i className="fas fa-star"></i>
+				   		Reviews ***** ({props.reviews.length}) 
+				   	</th>
+				  </tr>
+			  </thead>
+			  <tbody> 
+				  {props.reviews.map((review, i) => {
+				  	return (<ReviewListEntry key={i} review={review}/>)
+				  })}
+				</tbody>
+			</table>
 		</div>
-  )
+  );
 }
 
 export default ReviewList;
