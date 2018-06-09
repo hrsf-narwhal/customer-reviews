03f99d07353b67d5fa6568280fb116c9044e9344
Aigerim <aigerim.zholmurzayeva@gmail.com>
2018-06-08 21:42:06 -0700

    Add search bar

--- client/components/ReviewListEntry.jsx
+++ client/components/ReviewListEntry.jsx
@@ -13,11 +13,11 @@ const ReviewListEntry = (props) => {
 	return (
 			<tr>
 			 <div className={style.bottomBorder}>	 	
-			  <td className={style.date}>{props.review.date}</td>  		 
-				<img src="https://www.etsy.com/images/avatars/default_avatar_75x75.png"/>	
-				<ReactStars className={style.star}  value={props.review.stars} edit={false}/>
-	      <td className={style.review}>{props.review.review}</td>
-				<span className={style.customer}>Reviewed by <br/> {props.review.name}</span> 
+				  <td className={style.date}>{props.review.date}</td>  		 
+					<img src="https://www.etsy.com/images/avatars/default_avatar_75x75.png"/>	
+					<ReactStars className={style.star}  value={props.review.stars} edit={false}/>
+		      <td className={style.review}>{props.review.review}</td>
+					<span className={style.customer}>Reviewed by <br/> {props.review.name}</span> 
 				</div>	
 			</tr>
 	)
